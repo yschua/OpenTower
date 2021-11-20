2c3cc493f029da3a7c4302659795cd3696f2e4e5
Ys <4900895+yschua@users.noreply.github.com>
2021-11-20 21:56:46 +1100

    Draw prototype test tower

--- objects/Room.lua
+++ objects/Room.lua
@@ -1,29 +1,29 @@
 local Room = class('Room')
 
 Room.static.nextId = 1
-Room.static.allRooms = {}
 
-function Room:initialize(type, blockX, blockY)
+-- TODO rename to FloorSpace
+
+function Room:initialize(type, x, y)
   self.type = type
-  self.blockX = blockX
-  self.blockY = blockY
+  self.x = x
+  self.y = y
   self.blockWidth = 3
   self.id = Room.static.nextId
   Room.static.nextId = Room.static.nextId + 1
-  Room.static.allRooms[self.id] = self
 end
 
 function Room:draw()
-  local x = utils.toWorldX(self.blockX)
-  local y = utils.toWorldY(self.blockY)
+  local wx = utils.toWorldX(self.x)
+  local wy = utils.toWorldY(self.y)
   local width = self.blockWidth * c.BLOCK_SIZE
   local height = c.BLOCK_SIZE
 
   love.graphics.setColor(1, 1, 1)
-  love.graphics.rectangle('fill', x, y, width, height)
+  love.graphics.rectangle('fill', wx, wy, width, height)
   love.graphics.setColor(0, 0, 0)
-  love.graphics.rectangle('line', x, y, width, height)
-  love.graphics.print(self.type, x + 2, y + 2)
+  love.graphics.rectangle('line', wx, wy, width, height)
+  love.graphics.print(self.type, wx + 2, wy + 2)
 end
 
 return Room
--- tower.lua
+++ tower.lua
@@ -1,41 +1,65 @@
 local Floor = require 'objects.Floor'
 local Room = require 'objects.Room'
+local Stair = require 'objects.Stair'
+local Elevator = require 'objects.Elevator'
 
 local tower = {}
 
 local _floors = {}
-local _blocks = {}
-
-local function toBlockLevel(level)
-  return level + c.GROUND_BLOCK_HEIGHT + 1
-end
+local _map = {}
+local _rooms = {}
+local _movers = {}
 
 function tower.load()
-  for h = 1, c.WORLD_BLOCK_HEIGHT do
-    _blocks[h] = {}
-    for w = 1, c.WORLD_BLOCK_WIDTH do
-      _blocks[h][w] = 0
+  for y = 1, c.WORLD_BLOCK_HEIGHT do
+    _map[y] = {}
+    for x = 1, c.WORLD_BLOCK_WIDTH do
+      _map[y][x] = {roomId = 0, moverId = 0}
     end
   end
 
   -- TODO temp hard coded build
-  local blockLeft = 4
-  local blockRight = 15
-  for level = 0, 3 do
-    local blockLevel = toBlockLevel(level)
-    _floors[blockLevel] = Floor(blockLevel, blockLeft, blockRight)
+  local leftX = 4
+  local rightX = 15
+  for y = 11, 14 do
+    _floors[y] = Floor(y, leftX, rightX)
   end
-  tower.buildRoom('office', 5, 1)
-  tower.buildRoom('office', 8, 1)
-  tower.buildRoom('office', 12, 1)
+  tower.buildRoom('office', 5, 12)
+  tower.buildRoom('office', 8, 12)
+  tower.buildRoom('office', 12, 12)
+  tower.buildMover(Stair(6, 11))
+  local elevator = Elevator(11, 11)
+  tower.buildMover(elevator)
+  tower.setElevatorRange(elevator.id, 11, 13)
+  -- TODO also need to actually connect the elevator
 end
 
-function tower.buildRoom(type, blockX, level)
-  local blockLevel = toBlockLevel(level)
-  local room = Room(type, blockX, blockLevel)
-  _floors[blockLevel]:add(room)
-  for x = blockX, blockX + room.blockWidth - 1 do
-    _blocks[blockLevel][x] = room.id
+function tower.buildRoom(type, x, y)
+  local room = Room(type, x, y)
+  _rooms[room.id] = room
+  _floors[y]:add(room)
+  for ix = x, x + room.blockWidth - 1 do
+    _map[y][ix].roomId = room.id
+  end
+  return room.id
+end
+
+function tower.buildMover(mover)
+  assert(mover.class.super.name == 'Mover')
+  _movers[mover.id] = mover
+  _map[mover.y][mover.x].moverId = mover.id
+end
+
+function tower.setElevatorRange(id, bottomY, topY)
+  local mover = _movers[id]
+  assert(mover.class == Elevator)
+  for y = mover.bottomY, mover.topY do
+    _map[y][mover.x].moverId = 0
+  end
+  mover.bottomY = bottomY
+  mover.topY = topY
+  for y = mover.bottomY, mover.topY do
+    _map[y][mover.x].moverId = mover.id
   end
 end
 
@@ -48,20 +72,32 @@ function tower.draw()
     floor:draw()
   end
 
-  love.graphics.setColor(0, 0, 0)
-  for y, row in ipairs(_blocks) do
-    for x, id in ipairs(row) do
-      if id ~= 0 then
-        local font       = love.graphics.getFont()
-        local textWidth  = font:getWidth(id)
+  for y, row in ipairs(_map) do
+    for x, block in ipairs(row) do
+      if block.roomId ~= 0 then
+        local font = love.graphics.getFont()
+        local textWidth = font:getWidth(block.roomId)
         local textHeight = font:getHeight()
+        love.graphics.setColor(0, 0, 0)
         love.graphics.print(
-          id,
+          block.roomId,
           utils.toWorldX(x) + c.BLOCK_SIZE / 2,
           utils.toWorldY(y) + c.BLOCK_SIZE / 2,
           0, 1, 1,
           textWidth / 2,
           textHeight / 2)
+      end
+
+      if block.moverId ~= 0 then
+        local font = love.graphics.getFont()
+        local textWidth = font:getWidth(block.roomId)
+        local textHeight = font:getHeight()
+        local wx = utils.toWorldX(x) + c.BLOCK_SIZE / 2
+        local wy = utils.toWorldY(y)
+        love.graphics.setColor(0, 0, 0)
+        love.graphics.rectangle('fill', wx - textWidth / 2, wy - textHeight / 2, textWidth, textHeight)
+        love.graphics.setColor(1, 1, 1)
+        love.graphics.print(block.moverId, wx, wy, 0, 1, 1, textWidth / 2, textHeight / 2)
       end
     end
   end
